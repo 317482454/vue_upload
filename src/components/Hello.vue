a4450f81709db1cc705cb2e41f0276c36ebabcff
317482454@qq.com <317482454@qq.com>
2017-09-05 17:33:10 +0800

    添加webkit内核下文件夹读取文件，ie不进行处理

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -29,66 +29,106 @@
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      imgList: [],
-      size: 0
-    }
-  },
-  methods: {
-    fileClick(){
-      document.getElementById('upload_file').click()
-    },
-    fileChange(el){
-      if (!el.target.files[0].size) return;
-      this.fileList(el.target.files);
-      el.target.value = ''
-    },
-    fileList(files){
-      for (let i = 0; i < files.length; i++) {
-        this.fileAdd(files[i]);
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        imgList: [],
+        size: 0
       }
     },
-    fileAdd(file){
-      this.size = this.size + file.size;//总大小
-      let reader = new FileReader();
-      reader.vue = this;
-      reader.readAsDataURL(file);
-      reader.onload = function () {
-        file.src = this.result;
-        this.vue.imgList.push({
-          file
-        });
+    methods: {
+      fileClick() {
+        document.getElementById('upload_file').click()
+      },
+      fileChange(el) {
+        if (!el.target.files[0].size) return;
+        this.fileList(el.target);
+        el.target.value = ''
+      },
+      fileList(fileList) {
+        let files = fileList.files;
+        for (let i = 0; i < files.length; i++) {
+          //判断是否为文件夹
+          if (files[i].type != '') {
+            this.fileAdd(files[i]);
+          } else {
+            //文件夹处理
+            this.folders(fileList.items[i]);
+          }
+        }
+      },
+      //文件夹处理
+      folders(files) {
+        let _this = this;
+        //判断是否为原生file
+        if (files.kind) {
+          files = files.webkitGetAsEntry();
+        }
+        files.createReader().readEntries(function (file) {
+          for (let i = 0; i < file.length; i++) {
+            if (file[i].isFile) {
+              _this.foldersAdd(file[i]);
+            } else {
+              _this.folders(file[i]);
+            }
+          }
+        })
+      },
+      foldersAdd(entry) {
+        let _this = this;
+        entry.file(function (file) {
+          _this.fileAdd(file)
+        })
+      },
+      fileAdd(file) {
+        //总大小
+        this.size = this.size + file.size;
+
+        //判断是否为图片文件
+        if (file.type.indexOf('image') == -1) {
+          file.src = 'static/wenjian.png';
+          this.imgList.push({
+            file
+          });
+        } else {
+          let reader = new FileReader();
+          reader.vue = this;
+          reader.readAsDataURL(file);
+          reader.onload = function () {
+            file.src = this.result;
+            this.vue.imgList.push({
+              file
+            });
+          }
+        }
+      },
+      fileDel(index) {
+        this.size = this.size - this.imgList[index].file.size;//总大小
+        this.imgList.splice(index, 1);
+      },
+      bytesToSize(bytes) {
+        if (bytes === 0) return '0 B';
+        let k = 1000, // or 1024
+          sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+          i = Math.floor(Math.log(bytes) / Math.log(k));
+        return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+      },
+      dragenter(el) {
+        el.stopPropagation();
+        el.preventDefault();
+      },
+      dragover(el) {
+        el.stopPropagation();
+        el.preventDefault();
+      },
+      drop(el) {
+        el.stopPropagation();
+        el.preventDefault();
+        this.fileList(el.dataTransfer);
       }
-    },
-    fileDel(index){
-      this.size = this.size - this.imgList[index].file.size;//总大小
-      this.imgList.splice(index, 1);
-    },
-    bytesToSize(bytes){
-      if (bytes === 0) return '0 B';
-      let k = 1000, // or 1024
-        sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-        i = Math.floor(Math.log(bytes) / Math.log(k));
-      return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
-    },
-    dragenter(el){
-      el.stopPropagation();
-      el.preventDefault();
-    },
-    dragover(el){
-      el.stopPropagation();
-      el.preventDefault();
-    },
-    drop(el){
-      el.stopPropagation();
-      el.preventDefault();
-      this.fileList(el.dataTransfer.files);
     }
   }
-}
 </script>
 <style scoped>
   .upload_warp_img_div_del {
@@ -189,7 +229,8 @@ export default {
     box-shadow: 0px 1px 0px #ccc;
     border-radius: 4px;
   }
-  .hello{
+
+  .hello {
     width: 650px;
     margin-left: 34%;
   }
